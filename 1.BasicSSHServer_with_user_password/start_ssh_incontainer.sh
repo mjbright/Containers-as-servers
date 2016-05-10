4cd4a9fee6e710c92ad1f6d9f273762725a1417b
Mike Bright <github@mjbright.net>
2016-05-10 21:38:24 +0200

    Adapted to run on Alpine or Ubuntu Linux

--- 1.BasicSSHServer_with_user_password/start_ssh_incontainer.sh
+++ 1.BasicSSHServer_with_user_password/start_ssh_incontainer.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-service ssh start
+      #{ echo "Starting sshd on Alpine Linux"; /etc/init.d/sshd start; } ||
 
-bash
+[ -f /etc/alpine-release ] && {
+      while [ 1 ]; do
+        echo "Starting sshd on Alpine Linux";
+        /usr/sbin/sshd -d -D -f /etc/ssh/sshd_config;
+      done
+    } ||
+    {
+      echo "Starting sshd on Ubuntu Linux";
+      service ssh start;
+      bash
+    }
+
 
 
 
