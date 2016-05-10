3c174924a21dbb99a25a200cc45b59c0cbdfc7e4
Mike Bright <github@mjbright.net>
2016-05-10 21:40:33 +0200

    Greatly improved error checking, ssh port testing, runs on Alpine or Ubuntu

--- 1.BasicSSHServer_with_user_password/run.sh
+++ 1.BasicSSHServer_with_user_password/run.sh
@@ -7,6 +7,20 @@ die() {
     exit 1
 }
 
+waitOnPort() {
+    HOST=$1; shift;
+    PORT=$1; shift;
+
+    WAIT=2
+    SLEEP=1
+
+    echo "Waiting for port $PORT to open on host $HOST"
+    while ! nc -v -w $WAIT $HOST $PORT </dev/null >/dev/null 2>&1; do
+        echo "Retrying ... $HOST:$PORT"; sleep $SLEEP;
+    done
+    echo "Open"
+}
+
 if [ "$1" = "stop" ];then
     IDS=$(docker ps -q); docker stop $IDS; docker rm $IDS
     exit 0
@@ -19,16 +33,6 @@ KEY=~/.ssh/id_rsa
     ls -altr ${KEY}*
 }
 
-docker ps | grep container && die "Containers already running"
-
-#CONTAINER_ID1=$(docker run -itd $IMAGE "service ssh start; bash")
-#for num in 1 2 3;do
-#done
-CONTAINER_ID1=$(docker run --name container1 -h container1 -itd $IMAGE "/start_ssh_incontainer.sh")
-CONTAINER_ID2=$(docker run --name container2 -h container2 -itd $IMAGE "/start_ssh_incontainer.sh")
-CONTAINER_ID3=$(docker run --name container3 -h container3 -itd $IMAGE "/start_ssh_incontainer.sh")
-#CONTAINER_ID=$(docker run -itd $IMAGE bash)
-
 getIP() {
     CONTAINER_ID=$1
     
@@ -39,16 +43,52 @@ getIP() {
         grep \"IPAddress | tail -1 | sed -e 's/.*: "//' -e 's/".*//'
 }
 
-set -x
+#docker ps | grep container && die "Containers already running"
+
+docker ps | grep container1 && {
+    echo "Container1 already running"
+    CONTAINER_ID1=$(docker ps | grep container1 | awk '{print $1;}')
+} || {
+    CONTAINER_ID1=$(docker run --name container1 -h container1 -itd $IMAGE "/start_ssh_incontainer.sh")
+}
+
+docker ps | grep container2 && {
+    echo "Container2 already running"
+    CONTAINER_ID2=$(docker ps | grep container2 | awk '{print $1;}')
+} || {
+    CONTAINER_ID2=$(docker run --name container2 -h container2 -itd $IMAGE "/start_ssh_incontainer.sh")
+}
+
+docker ps | grep container3 && {
+    echo "Container3 already running"
+    CONTAINER_ID3=$(docker ps | grep container3 | awk '{print $1;}')
+} || {
+    CONTAINER_ID3=$(docker run --name container3 -h container3 -itd $IMAGE "/start_ssh_incontainer.sh")
+}
+
+#set -x
 IP1=$(getIP $CONTAINER_ID1)
 IP2=$(getIP $CONTAINER_ID2)
 IP3=$(getIP $CONTAINER_ID3)
 
-sleep 10
+[ -z "$IP1" ] && die "Failed to get ip address of $CONTAINER_ID1)"
+[ -z "$IP2" ] && die "Failed to get ip address of $CONTAINER_ID2)"
+[ -z "$IP3" ] && die "Failed to get ip address of $CONTAINER_ID3)"
 
-ssh -t user@$IP1 "sudo hostname"
-ssh -t user@$IP2 "sudo hostname"
-ssh -t user@$IP3 "sudo hostname"
-set +x
+waitOnPort $IP1 22
+waitOnPort $IP2 22
+waitOnPort $IP3 22
+
+ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $IP1
+ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $IP2
+ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R $IP3
+
+#ssh -t user@$IP1 "sudo hostname"
+#ssh -t user@$IP2 "sudo hostname"
+#ssh -t user@$IP3 "sudo hostname"
+ssh user@$IP1 "sudo hostname"
+ssh user@$IP2 "sudo hostname"
+ssh user@$IP3 "sudo hostname"
+#set +x
 
 
